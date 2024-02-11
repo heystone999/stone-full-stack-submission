3960c1c3d915e7ada690302ae2a7214dbe5d6d6d
heystone999 <52831724+heystone999@users.noreply.github.com>
2024-02-11 16:29:59 -0500

    feat: Exercises 4.8

---
+++ part4/bloglist/tests/bloglist_api.test.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const helper = require('./test_helper')
+const app = require('../app')
+const api = supertest(app)
+
+const Blog = require('../models/blog')
+
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+
+  for (let blog of helper.initialBlogs) {
+    let blogObj = new Blog(blog)
+    await blogObj.save()
+  }
+})
+
+
+describe('GET /api/blogs', () => {
+  test('blogs are returned as JSON', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('COntent-Type', /application\/json/)
+  }, 100000)
+
+  test('all notes are returned', async () => {
+    const response = await api.get('/api/blogs')
+    expect(response.body).toHaveLength(helper.initialBlogs.length)
+  })
+})
+
+
+afterAll(async () => {
+  await mongoose.connection.close()
+})
