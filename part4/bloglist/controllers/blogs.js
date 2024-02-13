61b55acc19e29f8406aa8cdfd6e3717251e7e793
heystone999 <52831724+heystone999@users.noreply.github.com>
2024-02-13 00:07:06 -0500

    feat: Exercises 4.20

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -10,19 +10,10 @@ blogsRouter.get('/', async (request, response) => {
   response.json(blogs)
 })
 
-
-const getTokenFrom = request => {
-  const authorization = request.get('authorization')
-  if (authorization && authorization.startsWith('Bearer ')) {
-    return authorization.replace('Bearer ', '')
-  }
-  return null
-}
-
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
 
-  const decodeToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  const decodeToken = jwt.verify(request.token, process.env.SECRET)
   if (!decodeToken.id) {
     return response.status(401).json({ error: 'token invalid' })
   }
